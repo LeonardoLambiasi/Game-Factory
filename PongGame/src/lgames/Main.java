f291a3060d851425dd9abc52e9248acc6bd755b3
LeonardoLambiasi <leoaml43@gmail.com>
2013-10-14 23:12:04 -0300

    tela de apresentacao

--- PongGame/src/lgames/Main.java
+++ PongGame/src/lgames/Main.java
@@ -3,13 +3,14 @@ package lgames;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration gcf = new LwjglApplicationConfiguration();
-		gcf.useGL20 = false;
-		gcf.title = "jogo";
-		gcf.width = 1080;
-		gcf.height = 720;
+		gcf.useGL20 = true;
+		gcf.title = "Pong Game";
+		gcf.width = 720;
+		gcf.height = 540;
 		
 		new LwjglApplication(new Pong(), gcf);
 	}
--- PongGame/src/lgames/SplashScreen.java
+++ PongGame/src/lgames/SplashScreen.java
@@ -1,23 +1,39 @@
 package lgames;
 
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenEquation;
+import aurelienribon.tweenengine.TweenEquations;
+import aurelienribon.tweenengine.TweenManager;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SplashScreen implements Screen{
 	private Texture splashTexture;
-	private Sprite splashSprite; 
-
-	@Override
+	private Sprite splashSprite;
+	private SpriteBatch batch;
+	private Pong game;
+	private TweenManager manager;
+	
+	public SplashScreen(Pong game) {
+		this.game = game;
+	}
+	
 	public void render(float delta) {
-		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		manager.update(delta);
 		
+		batch.begin();
+		splashSprite.draw(batch);
+		batch.end();
 	}
 
-	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -25,32 +41,34 @@ public class SplashScreen implements Screen{
 	public void show() {
 		splashTexture= new Texture("nintendologo.jpg");
 		
+		splashSprite = new Sprite(splashTexture);
+		splashSprite.setPosition(150, 110);
+		splashSprite.setColor(1, 1, 1, 0);
+		batch = new SpriteBatch();
+		
+		Tween.registerAccessor(Sprite.class, new SpriteTween());
+		manager = new TweenManager();
+		Tween.to(splashSprite, SpriteTween.ALPHA, 3f).target(1).ease(TweenEquations.easeInQuad).start(manager);
 		
 	}
 
-	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
 		
 	}
+
+	
 
 }
 	
